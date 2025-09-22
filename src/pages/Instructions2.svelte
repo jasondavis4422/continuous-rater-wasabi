9cafab79e0fa2fed5c4338c632f509fa6588284c
Jason Davis <jasondavis@default-10-231-131-131.vpnuser.dartmouth.edu>
2025-09-22 13:50:15 -0400

    Added emotion descriptions to the experiment.

--- src/pages/Instructions2.svelte
+++ src/pages/Instructions2.svelte
@@ -6,9 +6,23 @@
   export let ratingType;
 
   // Add/remove items here to create more instructions pages
-  
+  let description = '';
+if (ratingType === 'aroused') {
+  description = '<p>Aroused is defined as how energized or stimulated you feel in response to the movie. Examples of items that make one feel more aroused are fast, upbeat music, going on a rollercoaster, or interviewing for a job. Note that feeling aroused is not inherently characterized as good or bad. Rather, if you experience a change in how energized you are (regardless of if it is good or bad), then please change your rating accordingly. </p>'
+}
+ else if (ratingType === 'happy') {
+  description = '<p>Happy is defined as a general level of enjoyment or contentment to the movie. Examples of items that may reflect happiness are hanging out with friends, engaging in enjoyable hobbies or pursuing a meaningful goal. If you experience a change in your level of contentment to the movie, please change your rating accordingly.</p>';
+} else if (ratingType === 'calm') {
+  description = '<p>Calm is defined as how passive or unenergized you feel (a lack of energy or stimulation) in response to the movie. Examples that make one feel calmer are gray, dreary weather, reading a book, or doing some meditation. Note that feeling calm is not inherently characterized as good or bad. Rather, if you feel more passive and low-energy in response to the movie, then please change your rating accordingly. </p>';  
+} else if (ratingType === 'pleasant') {
+  description = '<p>Pleasant is defined as your moment-to-moment experience of delight or gratification to the movie. Examples of items that are pleasurable are having a good meal, receiving a compliment from a friend, or acquiring money. If you experience a change in your level of delight to the movie, please change your rating accordingly.</p>';
+} else if (ratingType === 'funny') {
+  description = '<p>Funny is defined as your experience of how humorous the movie was to you. Examples of things that may be funny are a witty joke, ridiculous movement of the body, or a creative pun. If you experience a change in your level of amusement to the movie, please change your rating accordingly.</p>';
+} else if (ratingType === 'unpleasant') {
+  description = '<p>Unpleasant is defined as your moment-to-moment experience of aversion or distaste to the movie. Examples of things that may be unpleasant are a mean comment said to you, a bad-tasting meal, or a loud, screeching noise. If you experience a change in your level of aversion to the movie, please change your rating accordingly.</p>';
+}
   const instructions = [
-    'Click the button below if you would like to proceed to the video. If you have already watched the first video clip, use the button again to proceed to the next video clip. Your video will appear shortly. </p> <p> Remember the corresponding keys: </p> <p> <strong>Up arrow key:</strong> move rating bar up</p><p><strong>Down arrow key:</strong> move rating bar down</p> </p> <p> <strong> REMEMBER: </strong> </p> <p> You are rating the following emotion: <strong> ' + ratingType + ' </strong> </p> <p> Please click the button below to proceed when you are ready. </p>'
+    'Click the button below if you would like to proceed to the video. If you have already watched the first video clip, use the button again to proceed to the next video clip. Your video will appear shortly. </p> <p> Remember the corresponding keys: </p> <p> <strong>Up arrow key:</strong> move rating bar up</p><p><strong>Down arrow key:</strong> move rating bar down</p> </p> <p> <strong> REMEMBER: </strong> </p> <p> You are rating the following emotion: <strong> ' + ratingType + ' </strong> </p> <strong> DESCRIPTION: </strong>'+ description + '<p> Please click the button below to proceed when you are ready. </p>'
   ];
 
   const dispatch = createEventDispatcher();
